686062b807e3f4ea24f687edf5a679c04d263003
Kiss Gergely <gergely.kiss5@stud.uni-corvinus.hu>
2021-12-29 14:18:16 +0100

    Exportálás #5

--- UserMaintenance/MintaZH/Form1.cs
+++ UserMaintenance/MintaZH/Form1.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MintaZH.Entities;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace MintaZH
 {
@@ -16,6 +18,12 @@ namespace MintaZH
     {
 
         List<OlympicsResult> results = new List<OlympicsResult>();
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -89,7 +97,7 @@ namespace MintaZH
                      orderby x.Year
                      select x.Year).Distinct().ToList();          
 
-            comboBox1.DataSource = Years;
+            comboBox1.DataSource = Years;            
         }
 
         //#4
@@ -139,6 +147,126 @@ namespace MintaZH
             {
                 o.Position = GetPosition(o);
             }
+        }
+
+        //#5 Excel export
+        /* Button>Click Excel generálás, feltöltés, kezelés átadás
+         * Tartalom: Kiválasztott év eredményei order by Position
+         * Header: Helyezés, Ország, Arany, Ezüst, Bronz
+         */
+
+        private void CreateExcel()
+        {
+            try
+            {   //Excel indítása
+                xlApp = new Excel.Application();
+                //Excel workbook létrehozása
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
+                //Új sheet létrehozása
+                xlSheet = xlWb.ActiveSheet;
+
+                CreateTable();
+
+                //Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nline: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                //Hiba esetén automatikus bezárás
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+
+            }
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Helyezés",
+                "Ország",
+                "Arany",
+                "Ezüst",
+                "Bronz"
+            };
+
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            int selectedYear = Convert.ToInt32(comboBox1.SelectedValue);
+            Console.WriteLine("Selected year: " + selectedYear);
+
+            List<OlympicsResult> filteredResults = (from x in results
+                                                    where x.Year == selectedYear
+                                                    orderby x.Position
+                                                    select x).ToList();
+
+            object[,] values = new object[filteredResults.Count, headers.Length];
+
+
+            int row = 0;
+            foreach (OlympicsResult f in filteredResults)
+            {
+
+                values[row, 0] = f.Position;
+                values[row, 1] = f.Country;
+                values[row, 2] = f.Medals[0];
+                values[row, 3] = f.Medals[1];
+                values[row, 4] = f.Medals[2];
+
+                row++;
+            };
+
+            Excel.Range r = xlSheet.Range[GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1))];
+
+            r.Value2 = values;
+
+
+            Excel.Range hdr = xlSheet.Range["A1", GetCell(1, values.GetLength(1))];
+
+            hdr.Font.Bold = true;
+            hdr.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            hdr.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            hdr.EntireColumn.AutoFit();
+            hdr.RowHeight = 40;
+            //hdr.Interior.Color = Color.LightBlue;
+            //hdr.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+
+        }
+
+
+        //#5 Trigger
+        public void ExcelGenerate(object sender, EventArgs e)
+        {
+            CreateExcel();
+            
         }
 
     }
